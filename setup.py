d95ecdeea342298c7222d62d1b17e72b17e6baa5
liyuangang <1095350436@qq.com>
2023-06-28 21:55:46 -0700

    prepare package and publish

--- setup.py
+++ setup.py
@@ -2,19 +2,6 @@ import setuptools
 import os
 
 
-# PROJECT_NAME = 'pyrclone'
-# if os.name == 'nt':  # Windows
-#     rclone_binary = (f'{PROJECT_NAME}', ['win/rclone.exe'])
-# elif os.name == 'posix':  # Unix-like
-#     if os.uname().sysname == 'Linux':
-#         rclone_binary = (f'{PROJECT_NAME}', ['linux/rclone'])
-#     elif os.uname().sysname == 'Darwin':
-#         rclone_binary = (f'{PROJECT_NAME}', ['mac/rclone'])
-# else:
-#     raise NotImplementedError('Unsupported OS')
-
-
-PROJECT_NAME = 'pyrclone'
 if os.name == 'nt':  # Windows
     rclone_binary = ['rclone.exe']
 elif os.name == 'posix':  # Unix-like
@@ -26,7 +13,7 @@ else:
     raise NotImplementedError('Unsupported OS')
 
 setuptools.setup(
-    name="pyrclone",
+    name="PyRClone",
     version="0.0.1",
     author="Yuangang Li",
     author_email="yuangangli@outlook.com",
@@ -34,10 +21,9 @@ setuptools.setup(
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/MrLYG/python-rclone.git",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=('mac', 'win','linux')),
     include_package_data=True,
     package_data={
-        # If any package contains *.bin files, include them:
         'pyrclone': rclone_binary,
     },
     classifiers=[
